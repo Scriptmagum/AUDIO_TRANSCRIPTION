61ac928f3c307537fba7be13454323a53e96a3db
usrlocal <usrlocal@GE22-FE17.ad.univ-angers.fr>
2026-01-14 14:55:49 +0100

    wav audio to text

--- SERVEUR/src/services/transcription.service.js
+++ SERVEUR/src/services/transcription.service.js
@@ -1,18 +1,36 @@
 import { pipeline } from "@xenova/transformers";
+import wavefile from 'wavefile';
+import fs from 'fs';
 
-await async function transcrireFichier(){
+async function transcrireFichier(){
 
-const transcriber = await pipeline('automatic-speech-recognition', 'Xenova/whisper-small');
+    const transcriber = await pipeline('automatic-speech-recognition', 'Xenova/whisper-small');
 
-console.log("Début transcription")
+    console.log("Début transcription")
 
-const result = await transcriber("", {  //mettre chevin vers là où on store le fichier audio
-    language : french,
-    chunk_lenght_s : 30,
-    stride_lenght_s : 5
-});
+    let buffer = fs.readFileSync('./audiofile.wav');
 
-console.log(result.text);
+    let wav = new wavefile.WaveFile(buffer);
+
+    wav.toBitDepth('32f');
+    wav.toSampleRate(16000);
+
+    let audioData = wav.getSamples();
+
+    if (Array.isArray(audioData)) {
+        if (audioData.length > 1) {
+            const monoData = audioData[0];
+            audioData = monoData; 
+        }
+    }
+
+    const result = await transcriber(audioData, {
+        language: 'french',
+        chunk_length_s: 30,
+        stride_length_s: 5
+    });
+
+    console.log(result.text);
 
 }
 
